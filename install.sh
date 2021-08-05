1b3ebd4817304d5c32446acd60445f53fac38ce5
Casper <casper@inernetbs.net>
2021-08-06 01:42:43 +0300

    Fix systemd service

--- install.sh
+++ install.sh
@@ -19,12 +19,12 @@ ExecStart=$UMBRELLA_PATH
 WantedBy=named.target
 " | sudo tee $SYSTEMD_SERVICE_UNIT_PATH > /dev/null 2>&1
   sudo chmod 664 $SYSTEMD_SERVICE_UNIT_PATH
-  sudo systemctl daemon-reload
-  sudo systemctl enable $SERVICE_NAME_FILE
   sudo mkdir -p /etc/systemd/system/named.service.d/
   echo "[Unit]
 Requires=umbrella.service
 " | sudo tee /etc/systemd/system/named.service.d/override.conf > /dev/null 2>&1
+  sudo systemctl daemon-reload
+  sudo systemctl enable $SERVICE_NAME_FILE
 fi
 
 if [ ! -f "$UMBRELLA_PATH" ]; then
