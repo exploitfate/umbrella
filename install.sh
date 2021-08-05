63c098fff0d49a2d4f8ae303060368a987b78c10
Casper <casper@inernetbs.net>
2021-08-05 23:23:51 +0300

    Fix installation

--- install.sh
+++ install.sh
@@ -13,16 +13,16 @@ ExecStart=$UMBRELLA_PATH
 
 [Install]
 WantedBy=default.target
-" | sudo tee $SYSTEMD_SERVICE_UNIT_PATH
+" | sudo tee $SYSTEMD_SERVICE_UNIT_PATH > /dev/null 2>&1
   sudo chmod 664 $SYSTEMD_SERVICE_UNIT_PATH
   sudo systemctl daemon-reload
   sudo systemctl enable $SERVICE_NAME_FILE
 fi
 
 if [ ! -f "$UMBRELLA_PATH" ]; then
-  sudo wget https://raw.githubusercontent.com/exploitfate/umbrella/main/umbrella.sh -O $UMBRELLA_PATH
+  sudo wget -nv -qO https://raw.githubusercontent.com/exploitfate/umbrella/main/umbrella.sh $UMBRELLA_PATH
   sudo chmod +x $UMBRELLA_PATH
 fi
 
 sudo apt update -qq
-sudo apt install -y dnsutils wget zip unzip parallel
+sudo apt install -q -y dnsutils wget zip unzip parallel
