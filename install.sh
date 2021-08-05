caa25ad782ac3e55ec971d5929903a37f0b893f0
Casper <casper@inernetbs.net>
2021-08-05 23:16:13 +0300

    Fix installation

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ SERVICE_NAME_FILE=umbrella.service
 SYSTEMD_SERVICE_UNIT_PATH=/etc/systemd/system/$SERVICE_NAME_FILE
 UMBRELLA_PATH=/usr/local/bin/umbrella.sh
 
-if [ ! -f $SYSTEMD_SERVICE_UNIT_PATH ]; then
-  echo '[Unit]
+if [ ! -f "$SYSTEMD_SERVICE_UNIT_PATH" ]; then
+  echo "[Unit]
 After=named.service
 
 [Service]
@@ -13,13 +13,13 @@ ExecStart=$UMBRELLA_PATH
 
 [Install]
 WantedBy=default.target
-' | sudo tee $SYSTEMD_SERVICE_UNIT_PATH
+" | sudo tee $SYSTEMD_SERVICE_UNIT_PATH
   sudo chmod 664 $SYSTEMD_SERVICE_UNIT_PATH
   sudo systemctl daemon-reload
   sudo systemctl enable $SERVICE_NAME_FILE
 fi
 
-if [ ! -f $UMBRELLA_PATH ]; then
+if [ ! -f "$UMBRELLA_PATH" ]; then
   sudo wget https://raw.githubusercontent.com/exploitfate/umbrella/main/umbrella.sh -O $UMBRELLA_PATH
   sudo chmod +x $UMBRELLA_PATH
 fi
